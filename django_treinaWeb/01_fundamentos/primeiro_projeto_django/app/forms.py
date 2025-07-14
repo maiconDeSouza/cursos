2445e3ba013a95b1014b5378958b2705fd6b73b6
Maicon Souza <maicon_souza09@hotmail.com>
2025-07-14 07:07:56 -0300

    14-07-25 07:07

--- django_treinaWeb/01_fundamentos/primeiro_projeto_django/app/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-
-class FormsClient(forms.Form):
-    name = forms.CharField(label='Nome do Cliente', max_length=200)
-    age = forms.IntegerField(label='Digite sua idade')
-    is_active = forms.BooleanField()
--- django_treinaWeb/01_fundamentos/primeiro_projeto_django/app/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-
-class Client(models.Model):
-    name = models.CharField(max_length=100)
-    age = models.IntegerField(null=True, blank=True)
-    is_active = models.BooleanField(default=True)
