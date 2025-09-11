18514413504cf4d29b78f89fde5fcd50c7125d69
Maicon Souza <maicon_souza09@hotmail.com>
2025-09-11 17:32:56 -0300

    init curso django

--- chatgpt_django/modulo_1/meuprojeto/paginainicial/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import HttpResponse
-
-
-# Create your views here.
-def index(request):
-    return HttpResponse('Bem-vindo ao meu primeiro projeto Django!')
--- go_gui_lopes/aula3/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/maiconDeSouza/cursos/tree/main/go_gui_lopes/aula3
-
-go 1.24.5
