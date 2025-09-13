30823d14ffd746c84edd472033484a99756b67bc
Maicon Souza <maicon_souza09@hotmail.com>
2025-09-13 17:35:04 -0300

    13-09-25 17:35

--- django_pro_do_zero_ao_deploy/my_blog/setup/settings.py
+++ django_pro_do_zero_ao_deploy/my_blog/setup/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,9 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    'django-insecure-)l#d)41^6%t)dj3#cft)r9p-rkc2l0eaui4=yud0c9k%tfne2_'
-)
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
