153b7068c1ed1824cff4df7d2e3df309f9ecac19
Maicon Souza <maicon_souza09@hotmail.com>
2025-09-13 22:13:12 -0300

    13-09-25 22:13

--- django_pro_do_zero_ao_deploy/my_blog/setup/settings.py
+++ django_pro_do_zero_ao_deploy/my_blog/setup/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_DIRS = [BASE_DIR / 'apps' / 'base' / 'static']
+STATICFILES_DIRS = [BASE_DIR / 'apps' / 'base' / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # media  files
