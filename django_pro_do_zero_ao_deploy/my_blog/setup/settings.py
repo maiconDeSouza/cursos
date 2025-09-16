8862e160ffdc7b116ade22086e830fbc2b9ecdb4
Maicon Souza <maicon_souza09@hotmail.com>
2025-09-16 15:45:39 -0300

    16-09-25 15:45

--- django_pro_do_zero_ao_deploy/my_blog/setup/settings.py
+++ django_pro_do_zero_ao_deploy/my_blog/setup/settings.py
@@ -26,11 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = [
-    '127.0.0.1',
-    'localhost',
-    '192.168.15.17',  # seu IP local
-]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.15.17', '192.168.15.123']
 
 
 # Application definition
