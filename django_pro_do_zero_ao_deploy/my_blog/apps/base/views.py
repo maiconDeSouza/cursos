b666f3d157572ec43f66c040e01a2417b2d9e394
Maicon Souza <maicon_souza09@hotmail.com>
2025-09-17 21:22:31 -0300

    17-09-25 21:22

--- django_pro_do_zero_ao_deploy/my_blog/apps/base/views.py
+++ django_pro_do_zero_ao_deploy/my_blog/apps/base/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.views import View
 
+from .models import Post
+
 
 # Create your views here.
 class Home(View):
     def get(self, resquest):
-        return render(resquest, 'base/pages/index.html')
+        posts = Post.objects.order_by('-created_at')[:3]
+        context = {'posts': posts}
+        return render(resquest, 'base/pages/index.html', context)
