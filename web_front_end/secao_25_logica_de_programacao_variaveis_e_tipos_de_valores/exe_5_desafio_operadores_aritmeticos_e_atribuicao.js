39cb6d286132fa3b8b22f3558823a99708b2f51c
Maicon Souza <maicon_souza09@hotmail.com>
2026-01-11 20:29:49 -0300

    11-01-26 20:30

---
+++ web_front_end/secao_25_logica_de_programacao_variaveis_e_tipos_de_valores/exe_5_desafio_operadores_aritmeticos_e_atribuicao.js
@@ -0,0 +1,10 @@
+let n = 10 
+n += 5
+n *= 5
+++n
+
+console.log(`O resta divisão ${n} por 6 é ${n % 6}`)
+console.log(`O quadrado do resto da divisão de ${n} por 6 é ${(n % 6) ** 2}`)
+console.log(n)
+n /= 4 
+console.log(`resultado final ${n}`)
---
+++ web_front_end/secao_25_logica_de_programacao_variaveis_e_tipos_de_valores/exe_6_exercico_NaN.js
@@ -0,0 +1,5 @@
+const var1 = 'Ba'
+const var2 = Number('a')
+const var3 = 'a'
+const result = `${var1}${var2}${var3}`
+console.log(result.toLowerCase())
