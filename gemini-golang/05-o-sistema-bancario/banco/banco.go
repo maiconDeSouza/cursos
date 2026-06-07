70969c25d118cfd62847346cec1e3f127bdb728a
Maicon Souza <maicon_souza09@hotmail.com>
2026-06-07 17:05:50 -0300

    07-06-28 17:05

--- gemini-golang/05-o-sistema-bancario/banco/banco.go
+++ gemini-golang/05-o-sistema-bancario/banco/banco.go
@@ -1,6 +1,12 @@
 package banco
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+)
+
+var conta = make(map[string]ContaBancaria)
 
 type ContaBancaria interface {
 	Sacar(valor float64) error
@@ -37,13 +43,13 @@ func VerificarSenha(c ContaBancaria, senha string) error {
 	return c.Senha(senha)
 }
 
-type ContaCorrente struct {
+type contaCorrente struct {
 	nome  string
 	saldo float64
 	senha string
 }
 
-func (c *ContaCorrente) Sacar(valor float64) error {
+func (c *contaCorrente) Sacar(valor float64) error {
 	total := valor + 2.00
 
 	if total > c.saldo {
@@ -56,8 +62,89 @@ func (c *ContaCorrente) Sacar(valor float64) error {
 	return nil
 }
 
-type Poupanca struct {
+func (c *contaCorrente) Depositar(valor float64) error {
+	c.saldo += valor
+
+	return nil
+}
+
+func (c contaCorrente) ObterSaldo() float64 {
+	return c.saldo
+}
+
+func (c contaCorrente) Senha(senha string) error {
+	if c.senha != senha {
+		err := errors.New("Senha inválida!")
+		return err
+	}
+
+	return nil
+}
+
+type poupanca struct {
 	nome  string
 	saldo float64
 	senha string
+}
+
+func (p *poupanca) Sacar(valor float64) error {
+	if valor > p.saldo {
+		err := errors.New("Saldo insuficiente")
+		return err
+	}
+
+	p.saldo -= valor
+
+	return nil
+}
+
+func (p *poupanca) Depositar(valor float64) error {
+	p.saldo += valor
+
+	return nil
+}
+
+func (p poupanca) ObterSaldo() float64 {
+	return p.saldo
+}
+
+func (p poupanca) Senha(senha string) error {
+	if p.senha != senha {
+		err := errors.New("Senha inválida!")
+		return err
+	}
+
+	return nil
+}
+
+func numeroDaConta() string {
+	n1 := (rand.Intn(100) + 1)
+	n2 := (rand.Intn(100) + 1)
+	n3 := (rand.Intn(100) + 1)
+	n4 := (rand.Intn(100) + 1)
+	n5 := (rand.Intn(100) + 1)
+
+	return fmt.Sprintf("%d%d%d%d%d", n1, n2, n3, n4, n5)
+
+}
+
+func CriarConta(nome string, tipoDaConta string, senha string) (map[string]ContaBancaria, string) {
+	var numeroNovaConta string
+	for {
+		numeroNovaConta = numeroDaConta()
+		_, existe := conta[numeroNovaConta]
+
+		if !existe {
+			break
+		}
+	}
+	switch tipoDaConta {
+	case "cc":
+		conta[numeroNovaConta] = &contaCorrente{nome: nome, saldo: 0, senha: senha}
+	case "cp":
+		conta[numeroNovaConta] = &poupanca{nome: nome, saldo: 0, senha: senha}
+	default:
+		conta[numeroNovaConta] = &poupanca{nome: nome, saldo: 0, senha: senha}
+	}
+	return conta, numeroNovaConta
 }
