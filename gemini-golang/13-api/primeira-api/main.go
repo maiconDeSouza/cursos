05ff2da292718f59da42f9afd8b79bfd4d0f7e05
Maicon Souza <maicon_souza09@hotmail.com>
2026-06-15 20:24:34 -0300

    15-06-26 20:24

--- gemini-golang/13-api/primeira-api/main.go
+++ gemini-golang/13-api/primeira-api/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"sync"
 )
 
 type Conta struct {
@@ -13,19 +14,58 @@ type Conta struct {
 
 type Cliente struct {
 	clientes []*Conta
+	gate     sync.RWMutex
 }
 
 func (cl *Cliente) addConta(co *Conta) {
+	cl.gate.Lock()
+	defer cl.gate.Unlock()
 	cl.clientes = append(cl.clientes, co)
 }
 
 func (cl *Cliente) listarContas() []*Conta {
+	cl.gate.RLock()
+	defer cl.gate.RUnlock()
 	return cl.clientes
+}
+
+func (cl *Cliente) getConta(titular string) *Conta {
+	cl.gate.RLock()
+	defer cl.gate.RUnlock()
+	contas := cl.clientes
+	var conta *Conta
+
+	for _, c := range contas {
+		if c.Titular == titular {
+			conta = c
+		}
+	}
+	return conta
 }
 
 var cl = Cliente{}
 
 func buscarConta(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Método não permitido", http.StatusMethodNotAllowed)
+		return
+	}
+
+	getName := r.URL.Query().Get("titular")
+
+	if getName != "" {
+		conta := cl.getConta(getName)
+		if conta == nil {
+			http.Error(w, "Conta não encontrada", http.StatusNotFound)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+
+		json.NewEncoder(w).Encode(conta)
+		return
+
+	}
+
 	c := cl.listarContas()
 
 	w.Header().Set("Content-Type", "application/json")
@@ -35,6 +75,11 @@ func buscarConta(w http.ResponseWriter, r *http.Request) {
 
 func criarConta(w http.ResponseWriter, r *http.Request) {
 	var novaConta Conta
+
+	if r.Method != http.MethodPost {
+		http.Error(w, "Método não permitido", http.StatusMethodNotAllowed)
+		return
+	}
 
 	err := json.NewDecoder(r.Body).Decode(&novaConta)
 
